4b6b7e31c1dd3d23df44a0d1f144876654c12434
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-04-22 19:56:49 +0200

    blank commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       <ButtonComponent/>
+      <ImageComponent src="https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif" alt="you got rick-rolled" className="medium-img"/>
       <ImageComponent src="https://images.unsplash.com/photo-1491884662610-dfcd28f30cfb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="a japanese tempe" className="medium-img"/>
       </header>
 
